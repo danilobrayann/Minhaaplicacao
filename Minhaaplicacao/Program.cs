ddadb23ff79a1d19230f15ffc4c021c5ae986157
Danilo Souza <danilobrayann@gmail.com>
2024-01-26 16:19:09 -0300

    feat: estudando c#

--- Minhaaplicacao/Program.cs
+++ Minhaaplicacao/Program.cs
@@ -10,6 +10,11 @@ namespace Minhaaplicacao
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("olá mundo");// aqui imprimi na tela uma informação
+            Console.ReadLine();// deixar a tela parada
+            Console.WriteLine(10+6);// soma de dois numeros
+            Console.ReadLine();// deixar a tela parada
+
         }
     }
 }
